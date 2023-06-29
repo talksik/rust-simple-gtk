360b71eed714f3472265bfeae134e2d5f190a8f5
talksik <patel.arjun50@gmail.com>
2023-06-29 09:14:37 -0700

    basic working helloworld

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,23 @@
+use gtk::{glib, prelude::*};
+use gtk::{Application, ApplicationWindow};
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> glib::ExitCode {
+    let app = Application::builder()
+        .application_id("org.example.HelloWorld")
+        .build();
+
+    app.connect_activate(|app| {
+        // We create the main window.
+        let window = ApplicationWindow::builder()
+            .application(app)
+            .default_width(320)
+            .default_height(200)
+            .title("Hello, World!")
+            .build();
+
+        // Show the window.
+        window.show();
+    });
+
+    app.run()
 }
